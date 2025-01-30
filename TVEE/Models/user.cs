bf5a08d3573854ec7bda4643247864db84f545fe
Firehiwot Getahun <rehiwott@gmail.com>
2025-01-30 01:47:57 -0800

    delete functionality corrected

--- TVEE/Models/user.cs
+++ TVEE/Models/user.cs
@@ -10,7 +10,8 @@ namespace Tvee.Models
         public string? Id { get; set; }  // MongoDB will auto-generate this
 
         public string? Username { get; set; }
-        
+        public string? PasswordHash { get; set; }
+        public string? Role { get; set; } // New field
         public string? Email { get; set; } // New field
         public string? Bio { get; set; } // New field
     }
